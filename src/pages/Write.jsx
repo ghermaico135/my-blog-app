de545920f280d8f08c7cc1cb0375020752175694
ghermaico135 <ghermaico135@gmail.com>
2025-02-18 04:12:37 -0800

    removing react quill

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import React from "react";
+// import ReactQuill from "react-quill";
+// import "react-quill/dist/quill.snow.css";
 
 function Write() {
   // const [value, setValue] = useState("");
