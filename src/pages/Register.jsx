86376788a59130440f68b205a78d819476777b03
ghermaico135 <ghermaico135@gmail.com>
2025-02-19 08:14:53 -0800

    Adding register front end page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios  from "axios"
 import "../Style/style.scss"
 import {Link} from "react-router-dom"
 
+
 function Register() {
+  const [inputs,setInputs] = useState({
+    username:"",email:"",password:""
+  })
+
+  const handleChange = (e) =>{
+    setInputs((prev)=>({ ...prev,[e.target.name] : e.target.value}))
+  }
+
+
+  const submitHandler = async (e) =>{
+    e.preventDefault()
+    try{
+      
+      // const res = await axios.post("http://localhost:5173/api/auth/register",inputs)
+      // console.log(res)
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+
   return (
     <div className="auth">
-    <h1>Login</h1>
+    <h1>Register</h1>
     <form>
-      <input type="text" placeholder='username' required/>
-      <input type="email" placeholder='Email' required/>
-      <input type="password" placeholder='password' required/>
-      <button>Register</button>
+      <input type="text" name="username" placeholder='username' required onChange={handleChange}/>
+      <input type="email" name="email" placeholder='Email' required onChange={handleChange}/>
+      <input type="password" name="password" placeholder='password' required  onChange={handleChange}/>
+      <button onClick={submitHandler}>Register</button>
       <span><p>If you have an account?</p> <Link className="link" to="/Login">Login here</Link></span>
     </form>
   </div>
